5cda9c4d3ebfa61340ee4ee15dd69bef8ec60338
Shyam Sundar <shyam.k@in.ey.com>
2025-06-19 07:37:48 +0000

    initial commit

---
+++ models/Snowflake/Sales.sql
@@ -0,0 +1,7 @@
+select id,sale_person_fname,sale_person_lname, sold_price, items_sold, country, region
+,case when product_name='Cell Phone' then 'Mobile' else product_name end as product_name
+,case when items_sold*sold_price >=3000 then 'High'
+    when items_sold*sold_price >=1500 then 'Medium'
+    else 'Low' end as totalsale_status
+from mydb.one_drive.total_sale
+order by sold_price desc 
